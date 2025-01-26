d2c468b46560e437d80c7224d5615f31f0ea9858
MeghKantariya <ml.kantariya@gmail.com>
2025-01-26 23:37:10 +0530

    Add files via upload

---
+++ ODDorEVEN.py
@@ -0,0 +1,9 @@
+#TO CHECK WHETHER THE NUMBER IS ODD OR NOT
+
+number=(input("Enter any no. = "))
+x=int(number)%2
+
+if (x==1):
+    print("The number entered is a ODD number")
+else:
+    print("The number entered is a EVEN number")
